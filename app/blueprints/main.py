dc77a648672ddb0aec00f8d8a195ce749e01255d
IsaacBrasil <isaac_brasil_oliveira@hotmail.com>
2023-10-16 12:25:59 -0300

    refatorando

--- app/blueprints/main.py
+++ app/blueprints/main.py
@@ -5,6 +5,13 @@ import os
 
 main = Blueprint('main', __name__)
 
+MIME_TYPES = {
+    "mp4": "video/mp4",
+    "avi": "video/x-msvideo",
+    "mkv": "video/x-matroska",
+    "flv": "video/x-flv"
+}
+
 # Modelo para metadados de vídeo no banco de dados
 class Video(db.Model):
     __tablename__ = 'video'
@@ -44,7 +51,24 @@ def show_upload():
 @main.route('/videos', methods=['GET'])
 def list_videos():
     videos = Video.query.all()
-    return jsonify([video.filename for video in videos]), 200
+    return render_template('video_list.html', videos=videos)
+    #return jsonify([video.filename for video in videos]), 200
+
+@main.route('/play/<int:video_id>', methods=['GET'])
+def play_video(video_id):
+    video = Video.query.get(video_id)
+    if not video:
+        abort(404)  # Se o vídeo não for encontrado, retorne um erro 404
+    
+    # Determinar o tipo MIME
+    file_extension = video.filename.rsplit('.', 1)[1].lower()  # Obtenha a extensão do arquivo
+    mime_type = MIME_TYPES.get(file_extension, "video/mp4")  # Use um tipo padrão se a extensão não for reconhecida
+    
+    return render_template('play_video.html', video=video, mime_type=mime_type)
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
